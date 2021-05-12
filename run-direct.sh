62dfdc8969a8232b7c08f6d74c46d2148f6760ac
Philipp Schuster <philipp.schuster@cyberus-technology.de>
2021-05-12 17:06:07 +0200

    fixed wrong comment

--- run-direct.sh
+++ run-direct.sh
@@ -8,9 +8,8 @@ QEMU_TO_IMG_DIR="./qemu-rt-img-direct"
 rm -rf $QEMU_TO_IMG_DIR
 mkdir -p "$QEMU_TO_IMG_DIR/EFI/BOOT"
 cp "startup.nsh" "$QEMU_TO_IMG_DIR/"
-# theoretically it would also work with "foobar.efi"
-# but "EFI/BOOT/BOOTX64.EFI" is the default bootloader by convention.
-# This can be found in section 3.4.1.1 in
+# iff the file is called "EFI/BOOT/BOOTX64.EFI" it get's invoked automatically.
+# This can be found in section 3.4.1.1 of
 # https://uefi.org/sites/default/files/resources/UEFI_Spec_2_9_2021_03_18.pdf
 cp "target/x86_64-unknown-uefi/debug/rust-hello-world-uefi-app.efi" "$QEMU_TO_IMG_DIR/EFI/BOOT/BOOTX64.EFI"
 
